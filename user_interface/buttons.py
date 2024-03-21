db244eb84812109985a603c50189e3eadc4efa5a
JanePrince666 <zhenyazav6666@gmail.com>
2024-03-21 18:37:50 +0400

    fixed the inline keyboard generator

--- user_interface/buttons.py
+++ user_interface/buttons.py
@@ -6,10 +6,11 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def make_row_callback_keyboard(items: dict) -> InlineKeyboardMarkup:
     buttons = []
     for item in items:
-        types.InlineKeyboardButton(
-            text=item[0],
-            callback_data=item[0]
-        )
+        buttons.append(
+            types.InlineKeyboardButton(
+                text=item,
+                callback_data=item
+            ))
 
-    keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[buttons])
     return keyboard
