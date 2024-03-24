763dbd8136b5fb438d819f18fd84486fed38951e
JanePrince666 <zhenyazav6666@gmail.com>
2024-03-24 14:01:06 +0400

    change inline keyboard

--- user_interface/buttons.py
+++ user_interface/buttons.py
@@ -1,16 +1,29 @@
 from aiogram import types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def make_row_callback_keyboard(items: dict) -> InlineKeyboardMarkup:
-    buttons = []
-    for item in items:
-        buttons.append(
-            types.InlineKeyboardButton(
-                text=item,
-                callback_data=item
-            ))
+# def make_row_callback_keyboard(items: dict) -> InlineKeyboardMarkup:
+#     buttons = []
+#     for item in items:
+#         button = InlineKeyboardButton(
+#             text=item,
+#             callback_data=item,
+#             width=1
+#         )
+#         buttons.append(button)
+#     keyboard = InlineKeyboardMarkup(inline_keyboard=[buttons], row_width=1)
+#
+#     return keyboard
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[buttons])
-    return keyboard
+
+def make_row_callback_keyboard(items: dict) -> InlineKeyboardMarkup:
+    keyboard = InlineKeyboardBuilder()
+    for item in items:
+        keyboard.button(
+            text=item,
+            callback_data=item,
+        )
+
+    keyboard.adjust(1)
+    return keyboard.as_markup()
