10813f5debda100fc66ad0ecf24e0919d1cbdc42
JanePrince666 <zhenyazav6666@gmail.com>
2024-03-28 01:16:22 +0400

    add comments and documentation to bot.py

--- bot.py
+++ bot.py
@@ -16,12 +16,16 @@ from db_management_OOP import PostingList
 from profiler import time_of_function
 
 
-# Функция постинга новых постов
+# Функция для отправки новых постов пользователям и удаления их из базы данных
 # @time_of_function
 async def post():
-    # print("начала постить")
+    # Устанавливаем соединение с базой данных
     connection = PostingList(*DATA_FOR_DATABASE)
+
+    # Получаем список новых постов
     new_posts = connection.get_posting_list()
+
+    # Отправляем каждый пост пользователям и удаляем его из списка
     for post_url, post_text, user_channel in new_posts:
         await bot.send_message(chat_id=int(user_channel), text=f"{post_url}\n{post_text}")
         connection.del_from_posting_list(post_url)
@@ -33,6 +37,7 @@ async def post():
 scheduler_for_posting = AsyncIOScheduler(timezone="Asia/Tbilisi")
 scheduler_for_posting.add_job(post, "interval", seconds=10)
 
+# Создаем задачу для параллельного запуска процесса получения новых постов
 t2 = multiprocessing.Process(target=get_new_posts)
 
 
@@ -41,11 +46,20 @@ async def main():
     # Включаем логирование, чтобы не пропустить важные сообщения
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
+    # Создаем диспетчер с хранилищем в памяти
     dp = Dispatcher(storage=MemoryStorage())
+
+    # Подключаем роутер для обработки команд
     dp.include_routers(commands.router)
+
+    # Подключаем роутеры для управления пользователями и каналами
     dp.include_router(management_user_channel.router)
     dp.include_router(management_watched_channel.router)
+
+    # Запускаем новый процесс для выполнения функции get_new_posts
     t2.start()
+
+    # Запускаем планировщик для публикации постов
     scheduler_for_posting.start()
     # Запускаем бота и пропускаем все накопленные входящие
     await dp.start_polling(bot)
